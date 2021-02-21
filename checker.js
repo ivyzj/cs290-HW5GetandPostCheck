4580a1e8e550dd8d76918fff75ba814ce7ee461e
IvyZheng85 <ivy.zheng85@gmail.com>
2021-02-21 15:22:29 -0800

    layout and js code
    layout should use table
    all data recieved in post request will be save in one variable: content

--- checker.js
+++ checker.js
@@ -11,30 +11,48 @@ app.set("port", 3000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
+app.get('/',function(req,res){
+    res.render('home');
+  });
+
+app.get("/get", function (req, res) {
     var getParams = [];
     for (var p in req.query) {
         getParams.push({ 'name': p, 'value': req.query[p] });
     }
     var context = {};
     context.dataList = getParams;
-    res.render("get", context);
+    res.render("getRequest", context);
 
 });
 
-app.post("/", function (req, res) {
-	var postParams = [];
+app.post("/post", function (req, res) {
+    var context = {}
+
+    // Data received in the request body
+	var bodyParams = [];
     for (var p in req.body) {
-        postParams.push({ 'name': p, 'value': req.body[p] });
-    }
-	var context = {};
-    context.dataList = postParams;
-    res.render("post", context);
+        bodyParams.push({ 'name': p, 'value': req.body[p] });
+    };
+
+    context.body_dataList = bodyParams;
+
+    // Data received in the request query
+    var queryParams = []
+    for (var q in req.query) {
+        queryParams.push({'name':q,'value':req.query[q]})
+    };
+    
+    context.query_dataList = queryParams;
+
+	res.render("postRequest", context);
+    
 });
 
-app.use(function (req, res) {
-    res.render("home", context);
-});
+// app.use(function (req, res) {
+//     res.render("home", context);
+// });
+
 
 app.use(function (req, res) {
     res.status(404);
